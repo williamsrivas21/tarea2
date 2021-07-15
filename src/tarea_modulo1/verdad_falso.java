1344da4d359359cc17dec591a4753b8d6253fc81
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 13:59:27 -0500

    tercer Comit para repositor remoto =

---
+++ src/tarea_modulo1/verdad_falso.java
@@ -0,0 +1,21 @@
+
+package tarea_modulo1;
+
+
+public class verdad_falso {
+     public static void main(String[] args) {
+     int M = 6;
+     int T = 1;
+     int K = -10;
+       
+     double divicion = (double)T/ (double)K;
+     int suma = M + T;
+     int resta = M-T;
+    
+      
+      
+      System.out.println("\nLa divicion es:"+divicion+"\nEl resultado de la suma es:"+suma+"\nLa resta es:"+ resta);
+    
+   
+    }
+}
