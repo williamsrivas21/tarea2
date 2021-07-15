5997a246cc216405bd3cf60398427445de5be58a
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 13:57:49 -0500

    tercer Comit para repositor remoto =

---
+++ src/tarea_modulo1/numeros_pares.java
@@ -0,0 +1,16 @@
+
+package tarea_modulo1;
+
+
+public class numeros_pares {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num= 1;
+        for (num = 1; num <=100; num++){
+            if (num%2 ==0){
+                System.out.println(num + "");
+            }
+        }
+    }
+    
+}
