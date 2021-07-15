0cbddad5cc9cf91e86aa8f7a4cc85b0657fdbde5
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 13:56:44 -0500

    tercer Comit para repositor remoto =

---
+++ src/tarea_modulo1/Tarea_modulo1.java
@@ -0,0 +1,14 @@
+
+package tarea_modulo1;
+
+
+public class Tarea_modulo1 {
+
+    
+    public static void main(String[] args)
+    {
+        
+     System.out.println("Hola, Soy de la clase de Programación Orientada a Objetos");
+    }
+    
+}
