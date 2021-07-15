0182b13cc8e9e10ea69c81a84b2e789e16d32bd6
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 13:58:37 -0500

    tercer Comit para repositor remoto =

---
+++ src/tarea_modulo1/operaciones.java
@@ -0,0 +1,18 @@
+
+package tarea_modulo1;
+
+
+public class operaciones {
+    public static void main(String[] args) {
+     int numero1 = 8;
+     int numero2 = 6;
+     
+     int suma = numero1+numero2;
+     int resta = numero1-numero2;
+     int multiplicacion = numero1*numero2;
+     double divicion = (double)numero1/ (double)numero2;
+     
+     System.out.println("El resultado de la suma es:"+suma+"\n La resta es:"+ resta+ "\n La multiplicacion es:"+multiplicacion+"\n La divicion es:"+divicion);
+    }
+    
+}
